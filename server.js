94e66b8fd67eb27d8d6f11ff601618adb608c97b
matyaspapp <project.matyas.papp@protonmail.com>
2020-09-08 10:31:43 +0200

    add database connection and push fnctionality

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import Videos from './dbModel.js';
+
+// app config
 const app = express();
 const port = 9000;
 
+// middlewares
+app.use(express.json());
+
+// db config
+const connectionUrl =
+  'mongodb+srv://admin:83db2LqYq3tBgGtl@cluster0.oxamp.mongodb.net/tiktok?retryWrites=true&w=majority';
+
+mongoose.connect(connectionUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+// api endpoints
 app.get('/', (req, res) =>
-  res.status(200).send('Hello world!')
+  res
+    .status(200)
+    .send({data: 'Hello world!'})
 );
 
+app.get('/v1/posts', (req, res) => 
+  res
+    .status(200)
+    .send(Data)
+);
+
+app.post('/v2/posts', (req, res) => {
+  const dbVideos = req.body;
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res
+        .status(500)
+        .send(err);
+    } else {
+      res
+        .status(201)
+        .send(data);
+    }
+  });
+});
+
+// listen
 app.listen(port, () =>
   console.log(`I am listening to you.. ${port}..`)
 );
